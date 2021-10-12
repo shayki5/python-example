a8703b7366ee39dec5b01ca6d9c9679707e54e34
Shayki Abramczyk <shayki5@gmail.com>
2021-10-12 13:36:28 +0300

    Update script.py

--- script.py
+++ script.py
@@ -1,3 +1,11 @@
+import os
 print("***************")
 print("Shayki Abramczyk")
 print("***************")
+
+
+input1 = os.getenv("String")
+input2 = os.getenv("Choice")
+
+print("input1")
+print("input2")
