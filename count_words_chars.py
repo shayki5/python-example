a7dd7d2a1f129000fcc31e005379b34b6d06df40
Shayki Abramczyk <shayki.abramczyk@tipalti.com>
2021-10-13 19:12:39 +0300

    separate init test to another file

--- count_words_chars.py
+++ count_words_chars.py
@@ -1,5 +1,3 @@
-import unittest
-
 def longest_word(file):
     with open(file, 'r') as infile:
                 words = infile.read().split()
@@ -7,10 +5,3 @@ def longest_word(file):
     longestWord = ([word for word in words if len(word) == max_len])
     print("Longest word is: {}. num of chars: {}".format(longestWord, len(longestWord[0])))
     return(len(longestWord[0]))
-
-class MyTest(unittest.TestCase):
-    def test(self):
-        self.assertGreater(longest_word('test.txt'), 9)
-
-if __name__ == '__main__':
-    unittest.main()
---
+++ unit_test.py
@@ -0,0 +1,9 @@
+from count_words_chars import longest_word
+import unittest
+
+class MyTest(unittest.TestCase):
+    def test(self):
+        self.assertGreater(longest_word('test.txt'), 9)
+
+if __name__ == '__main__':
+    unittest.main()
